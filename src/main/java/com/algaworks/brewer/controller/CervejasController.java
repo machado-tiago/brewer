44cef280b560f3d22055234e021aa517bcc3e76a
machado-tiago <tiagomachadox@gmail.com>
2020-09-22 00:24:35 -0300

    classe Cerveja Criada e dependência de validação

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -1,13 +1,29 @@
 package com.algaworks.brewer.controller;
 
+import javax.validation.Valid;
+
+import com.algaworks.brewer.model.Cerveja;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CervejasController {
     
     @GetMapping(value = "/cervejas/novo")
     public String novo() {
+        return "/cerveja/cadastroCerveja";
+    }
+
+    @PostMapping(value = "/cervejas/novo")
+    public String cadastrar(@ModelAttribute @Valid Cerveja cerveja, BindingResult result){
+        if (result.hasErrors()) {
+            System.out.println("Erro no preenchimento do formulário!");
+        }
+        System.out.println(cerveja.getSku());
         return "/cerveja/cadastroCerveja";
     }
 }
---
+++ src/main/java/com/algaworks/brewer/model/Cerveja.java
@@ -0,0 +1,27 @@
+package com.algaworks.brewer.model;
+
+import javax.validation.constraints.NotBlank;
+
+public class Cerveja {
+    @NotBlank
+    private String sku;
+    private String nome;
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+}
