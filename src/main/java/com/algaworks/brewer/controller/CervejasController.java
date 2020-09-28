e098e404bb611fe00f6c8447ee9819e320643cf5
machado-tiago <tiagomachadox@gmail.com>
2020-09-28 17:22:32 -0300

    Configuração logging

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
+
 @Controller
 public class CervejasController {
     
---
+++ src/main/java/com/algaworks/brewer/controller/EstiloController.java
@@ -0,0 +1,15 @@
+package com.algaworks.brewer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/estilos")
+public class EstiloController {
+    
+    @GetMapping(value = "/novo")
+    public String novo(){
+        return "estilo/cadastroEstilo";
+    }
+}
